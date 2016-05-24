fcf5a226690a15eb2bb53ca964a91474ce50d4f7
Filip Hallqvist <filip@raket.nu>
2016-05-24 14:22:10 +0200

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # Compress files
-zip -r wally-theme.zip . -x \*.git\* \*node_modules\* \*bower_components\* gulpfile.js deploy.sh wally-theme.zip .jshintrc package.json
+zip -r wally-theme.zip . -x \*.git\* \*node_modules\* \*bower_components\* gulpfile.js deploy.sh wally-theme.zip .jshintrc package.json \*.DS_Store\*
 
 # Upload to raket.nu ~/vhosts/wally-wp/dist/staging
 scp -i ~/.ssh/id_rsa wally-theme.zip raketnu@raket.nu:~/vhosts/wally-wp/dist/staging/wally-theme.zip
