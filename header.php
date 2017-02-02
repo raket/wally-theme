adeaf43b4d64788d06f5d4b66de83813c0a7ea3b
Gustav Arrhenius <gustavarrhenius@G-bags-Macbook-Pro.local>
2017-02-02 08:57:13 +0100

    Echo Blog description in header

--- header.php
+++ header.php
@@ -90,7 +90,7 @@ $turl = get_template_directory_uri();
                              alt="<?php bloginfo('name') ?>"/>
                     <?php endif ?>
                         <span><?php bloginfo('name') ?></span>
-                        <p><?php apply_filters('wally_header_description', get_bloginfo('description')) ?></p>
+                        <p><?php echo apply_filters('wally_header_description', get_bloginfo('description')) ?></p>
                 </a>
                 <div class="site-header__tools">
                     <ul>
