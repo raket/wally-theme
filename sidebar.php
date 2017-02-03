ac1f2665dbac653a16a51a7d3dc21f4780f80ab5
Gustav Arrhenius <gustavarrhenius@G-bags-Macbook-Pro.local>
2017-02-03 11:54:33 +0100

    Add filter to dynamic sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,15 +1,16 @@
 <div class="sidebar fw-col-md-4" aria-label="<?php _e('Sidomeny', 'wally-theme') ?>">
-    <?php
-    if(is_front_page() || is_home() || is_archive() || is_page_template('home.php')){
-        ob_start();
-        dynamic_sidebar( 'blog-sidebar' );
-        $sidebar_output = ob_get_contents();
-        ob_end_clean();
-        echo apply_filters('w_blog_sidebar_output', $sidebar_output);
-    } elseif(is_page()){
-        get_template_part('parts/navigation/subpages');
-    } elseif(is_single()){
-        get_template_part('parts/navigation/single');
-    }
-    ?>
+	<?php
+	$show_dynamic_sidebar = apply_filters( 'w_show_dynamic_sidebar', false );
+	if(is_front_page() || is_home() || is_archive() || is_page_template('home.php') || $show_dynamic_sidebar){
+		ob_start();
+		dynamic_sidebar( 'blog-sidebar' );
+		$sidebar_output = ob_get_contents();
+		ob_end_clean();
+		echo apply_filters('w_blog_sidebar_output', $sidebar_output);
+	} elseif(is_page()){
+		get_template_part('parts/navigation/subpages');
+	} elseif(is_single()){
+		get_template_part('parts/navigation/single');
+	}
+	?>
 </div>
